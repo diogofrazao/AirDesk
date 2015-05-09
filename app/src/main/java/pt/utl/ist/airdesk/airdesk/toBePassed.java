0c13d7b25903de127808ff2692f4eacb12b14a7e
ist170542 <duarte.baptista@ist.utl.pt>
2015-05-09 16:53:50 +0100

    f

--- app/src/main/java/pt/utl/ist/airdesk/airdesk/toBePassed.java
+++ app/src/main/java/pt/utl/ist/airdesk/airdesk/toBePassed.java
@@ -1,9 +1,13 @@
 package pt.utl.ist.airdesk.airdesk;
 
+import java.io.Serializable;
+
 /**
  * Created by duarte on 5/9/15.
  */
-public class toBePassed {
+public class toBePassed implements Serializable {
+
+
 
     String balda="xuxu";
 
